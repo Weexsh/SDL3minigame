707d693988687eeeb5b78a33b0109a99acb7a40d
Weexsh <user.email>
2026-07-27 16:13:40 +0800

    成功把圖片跟背景render上window

--- hello_cmake/miniGame.cpp
+++ hello_cmake/miniGame.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
 struct Player{
     SDL_Surface *mSurface;
     SDL_Texture *mTexture;
 
     Player(SDL_Renderer* r){
         mSurface=SDL_LoadBMP("./test.bmp");
+        if(!(mSurface)){
+            SDL_Log("surface創建失敗: %s",SDL_GetError());
+        }
+       
         mTexture=SDL_CreateTextureFromSurface(r,mSurface);
-    }
-    Player(){}
-    ~Player(){
+        if(!(mTexture)){
+            SDL_Log("texture創建失敗: %s",SDL_GetError());
+        }
         SDL_DestroySurface(mSurface);
+    }
+    Player(){
+        mTexture=nullptr;
+    }
+    ~Player(){
         SDL_DestroyTexture(mTexture);
     }
     void render(SDL_Renderer *r){
         SDL_FRect dst{
             .x=50,
             .y=50,
-            .w=30,
-            .h=30
+            .w=33,
+            .h=25
         };
         SDL_RenderTexture(r, mTexture, NULL, &dst);
     }
@@ -27,59 +37,61 @@ struct Player{
 struct SDLminiGame{
     SDL_Window *mWindow;
     SDL_Renderer *mRenderer;
-    Player player{mRenderer};
-
+    Player* player;
+    bool running = true;
+    
     SDLminiGame(){
+        
         SDL_Init(SDL_INIT_VIDEO);
         mWindow = SDL_CreateWindow("first miniGame", 500, 500, SDL_WINDOW_OPENGL);
-        
-        if(mWindow==nullptr){
-            SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        if(!(mWindow)){
+            SDL_Log("window創建失敗: %s",SDL_GetError());
         }
+        mRenderer=SDL_CreateRenderer(mWindow,nullptr);
+         player = new Player(mRenderer);
     }
     
     ~SDLminiGame(){
-        SDL_DestroyWindow(mWindow);
+        delete player;
         SDL_DestroyRenderer(mRenderer);
+        SDL_DestroyWindow(mWindow);
         SDL_Quit();
     }
-    
-    void Input(){
-        bool running = true;
-        const bool *key_states=SDL_GetKeyboardState(NULL);
+    void Tick(){
+        Input();
+        Render();
 
-        while (running) {
-            SDL_Event event;
-            while (SDL_PollEvent(&event)) {
-                if (event.type == SDL_EVENT_QUIT) { 
-                    SDL_Log("errror");
-                    running = false;
-                }
+    }
+    void Input(){
+        
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) { 
+                running = false;
             }
         }
     }
     void Render(){
-        SDL_SetRenderDrawColor(mRenderer, 0x00, 0xFF, 0xFF, 0xFF);
-
+        
         SDL_RenderClear(mRenderer);
 
-        SDL_RenderPresent(mRenderer);
+        SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0x00, 0xFF);
 
+        player->render(mRenderer);
+
+        SDL_RenderPresent(mRenderer);
     }
 
     void MainRun(){
-        Input();
-        Render();
-        player.render(mRenderer);
+        while(running){
+            SDL_Delay(16);
+            Tick();
+        }
     }
-        
 };
-int main(int argc,char*argv[]){
-    bool SDL_Init(SDL_InitFlags flags);
 
+int main(int argc,char*argv[]){
     SDLminiGame Game; 
-    
     Game.MainRun();
-    
     return 0;
 }
