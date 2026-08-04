3e3eaebf93b840369dba0105faae396278993df9
Weexsh <user.email>
2026-08-04 16:22:36 +0800

    加顯示體力條

--- hello_cmake/miniGame.cpp
+++ hello_cmake/miniGame.cpp
@@ -5,10 +5,11 @@
 struct Player{
     SDL_Surface *mSurface;
     SDL_Texture *mTexture;
+    float bar;
     float Tx,Ty,Tw,Th;;
     Player(SDL_Renderer* r){
         Tx=20,Ty=20,Tw=25,Th=33;
-
+        bar=1000;
         mSurface=SDL_LoadBMP("./test.bmp");
         if(!(mSurface)){
             SDL_Log("surface創建失敗: %s",SDL_GetError());
@@ -20,9 +21,7 @@ struct Player{
         }
         SDL_DestroySurface(mSurface);
     }
-    Player(){
-        mTexture=nullptr;
-    }
+    Player(){}
     ~Player(){
         SDL_DestroyTexture(mTexture);
     }
@@ -74,7 +73,6 @@ struct SDLminiGame{
     bool fullscreen=false;
 
     SDLminiGame(){
-        
         SDL_Init(SDL_INIT_VIDEO);
         mWindow = SDL_CreateWindow("first miniGame", 500, 500, SDL_WINDOW_OPENGL);
         if(!(mWindow)){
@@ -84,7 +82,6 @@ struct SDLminiGame{
         player = new Player(mRenderer);
         back = new scene(mRenderer);
     }
-    
     ~SDLminiGame(){
         delete back;
         delete player;
@@ -95,9 +92,6 @@ struct SDLminiGame{
     void Tick(){
         Input();
         Render();
-    }
-    void background(){
-
     }
     void Input(){
         const bool *key_states = SDL_GetKeyboardState(NULL);
@@ -111,24 +105,38 @@ struct SDLminiGame{
                     SDL_SetWindowFullscreen(mWindow,fullscreen);
                 }
             }
-
         }
         float speed = 1;
+        if(key_states[SDL_SCANCODE_LSHIFT]&&player->bar>0){
+            speed = 2;
+            player->bar-=10;
+        }
+        if(!(key_states[SDL_SCANCODE_LSHIFT])&&(player->bar<1000)){
+            player->bar+=10;
+        }
         
-        if(key_states[SDL_SCANCODE_LSHIFT])speed = 5;
         if(key_states[SDL_SCANCODE_W])player->move(0,-2,speed);
         if(key_states[SDL_SCANCODE_S])player->move(0,2,speed);
         if(key_states[SDL_SCANCODE_D])player->move(2,0,speed);
         if(key_states[SDL_SCANCODE_A])player->move(-2,0,speed);
     }
+    void RenderText(){
+        SDL_SetRenderScale(mRenderer, 4, 4);
+        SDL_SetRenderDrawColor(mRenderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderDebugTextFormat(mRenderer,20, 20, "energy: %.0f",player->bar/10.0f);
+        SDL_SetRenderScale(mRenderer,1,1);
+    }
     void Render(){
+        SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0x00, 0xFF);
         
         SDL_RenderClear(mRenderer);
 
-        SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0x00, 0xFF);
-        back->render(mRenderer);
+        back->render(mRenderer); 
+
         player->render(mRenderer);
 
+        RenderText();
+       
         SDL_RenderPresent(mRenderer);
     }
 
